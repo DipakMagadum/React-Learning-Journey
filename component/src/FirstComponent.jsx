4fdf4395129655de158517db0c6cb4d1a41f6ed0
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-27 22:46:53 +0530

    Created first component using JSX

---
+++ component/src/FirstComponent.jsx
@@ -0,0 +1,14 @@
+function FirstComponent(){
+    return(
+        <>
+        <h1>Unordered List</h1>
+        <ul>
+            <li>Html</li>
+            <li>Css</li>
+            <li>Javascript</li>
+            <li>React</li>
+        </ul>
+        </>
+    )
+}
+export default FirstComponent;

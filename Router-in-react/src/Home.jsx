d7cd903e1d2b23ab70fdf6bd232c3d39c73a7843
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-29 01:20:01 +0530

    style home.jsx using tailwindcss

--- Router-in-react/src/Home.jsx
+++ Router-in-react/src/Home.jsx
@@ -1,13 +1,15 @@
 export function Home(){
     return(
         <>
-        <nav className="flex bg-sky-600">
-            <ul>
+        <div className="flex bg-sky-600 h-15 items-center justify-end p-4 ">
+        <nav>
+            <ul className="flex gap-7 text-white text-lg">
                 <li>Login</li>
                 <li>Register</li>
                 <li>About</li>
             </ul>
         </nav>
+        </div>
         </>
     )
 }

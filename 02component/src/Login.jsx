22244b7d802a36a12593e7e7e37d9cfb66ff93ac
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-29 00:12:17 +0530

    added style into login.jsx

--- 02component/src/Login.jsx
+++ 02component/src/Login.jsx
@@ -3,15 +3,17 @@ export function Login(){
         <>
         <div className="flex justify-center items-center h-screen">
 
-            <form >
-                <h1>Login</h1>
+            <form className="flex flex-col bg-white shadow-xl/30 p-7 w-80 gap-4">
+                <h1 className="text-center text-green-800 font-bold text-xl">Login</h1>
                 <label>Username</label>
-                <input type="text" placeholder="username"/>
+                <input className="border p-1 rounded"
+                 type="text" placeholder="username"/>
 
                 <label>Password</label>
-                <input type="password" placeholder="password"/>
+                <input className="border p-1 rounded"
+                 type="password" placeholder="password"/>
 
-                <button>Login</button>
+                <button className="bg-green-700  text-white text-lg p-2 rounded-xl ">Login</button>
             </form>
         </div>
         </>

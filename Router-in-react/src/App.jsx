8e260f474e251e87e4983c4fd1e8704fc1cf1a9e
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-29 00:32:01 +0530

    installed and imported react-router-dom

--- Router-in-react/src/App.jsx
+++ Router-in-react/src/App.jsx
@@ -1,9 +1,10 @@
-
+import {Routes,Route} from "react-router-dom"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <Routes>
+      
+    </Routes>
     </>
   )
 }

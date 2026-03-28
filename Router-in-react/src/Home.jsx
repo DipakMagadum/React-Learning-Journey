10b1d3ba220902b832f3e7667b647f5d3e922bea
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-29 00:39:27 +0530

    added a navbar in home

---
+++ Router-in-react/src/Home.jsx
@@ -0,0 +1,13 @@
+export function Home(){
+    return(
+        <>
+        <nav>
+            <ul>
+                <li>Login</li>
+                <li>Register</li>
+                <li>About</li>
+            </ul>
+        </nav>
+        </>
+    )
+}

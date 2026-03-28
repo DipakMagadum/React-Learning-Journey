fc399f7fe21830e1e0eca43a63daefdebe48c47b
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-29 00:37:13 +0530

    import and render Home component in App

--- Router-in-react/src/App.jsx
+++ Router-in-react/src/App.jsx
@@ -1,9 +1,10 @@
 import {Routes,Route} from "react-router-dom"
+import {Home} from "./Home"
 function App() {
   return (
     <>
     <Routes>
-      
+      <Route path="/home" element={<Home/>}/>
     </Routes>
     </>
   )

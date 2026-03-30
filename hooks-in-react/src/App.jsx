59aa844b300f11e4d17310a27a422663beb3c8f9
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-30 21:45:47 +0530

    import Register.jsx in App.jsx

--- hooks-in-react/src/App.jsx
+++ hooks-in-react/src/App.jsx
@@ -1,10 +1,12 @@
 import {Login} from "./Login.jsx"
+import {Register} from "./Register.jsx"
 import {Routes,Route} from "react-router-dom"
 function App() {
   return (
    <>
    <Routes>
     <Route path="/" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
    </Routes>
    </>
   )

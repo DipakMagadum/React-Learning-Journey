1d6a3d7ffdfe7d9b84e6352625fc5b8b3693d02d
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-30 21:31:05 +0530

    update error message color using Tailwind CSS

--- hooks-in-react/src/Login.jsx
+++ hooks-in-react/src/Login.jsx
@@ -42,8 +42,10 @@ const [msg, setMsg] = useState("");
             <form  onSubmit={validate} noValidate
             
             className="flex flex-col bg-white shadow-xl/30 p-6 gap-4 w-70">
+
                 <h1 className="text-center text-sky-600 text-xl font-bold">Login</h1>
-                <p>{msg}</p>
+
+                <p className="text-center text-xl text-green-400">{msg}</p>
                 <label>Username</label>
                 <input className="border p-1 rounded "
                 type="text"
@@ -51,7 +53,7 @@ const [msg, setMsg] = useState("");
                 value={username}
                 onChange={(e)=>setUsername(e.target.value)}
                 />
-            <p>{error.username}</p>
+            <p className="text-red-700">{error.username}</p>
                 <label>Password</label>
                 <input  className="border p-1 rounded "
                 type="password"
@@ -59,7 +61,7 @@ const [msg, setMsg] = useState("");
                 value={password}
                 onChange={(e)=>setPass(e.target.value)}
                 />
-            <p>{error.password}</p>
+            <p className="text-red-700">{error.password}</p>
                 <button className="bg-sky-600 p-1 rounded-xl">Login</button>
             </form>
         </div>

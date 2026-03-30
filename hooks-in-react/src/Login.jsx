51b75a77543ea319727f080e498490540e434072
DipakMagadum <deepakmagdum769@gmail.com>
2026-03-30 21:19:44 +0530

    use Validattion on login page

--- hooks-in-react/src/Login.jsx
+++ hooks-in-react/src/Login.jsx
@@ -2,10 +2,46 @@ import {useState} from "react"
 export function Login(){
 const [username, setUsername] = useState("");
 const [password, setPass] = useState("");
+const [error, setError] = useState({username:"",password:""});
+const [msg, setMsg] = useState("");
+
+        validate=(e)=>{
+            e.preventDefault();
+
+            let NewError={username:"",password:""}
+            let validate=true;
+
+            if(username.trim() === ""){
+                NewError.username="Username Required !";
+                validate=false;
+            }
+            else if(username.length < 2){
+                NewError.username="Username atleast 4 char long";
+                validate=false;
+            }
+            if(password === ""){
+                NewError.password="Password Required !";
+                validate=false;
+            }
+            else if(password.length < 8){
+                NewError.password="Password must be 8 char long";
+                validate=false;
+            }
+            if(validate){
+                setMsg("Login Successful");
+            }
+            else{
+                setMsg("")
+            }
+            setError(NewError);
+        }
+
     return(
         <>
         <div className="flex justify-center items-center h-screen">
-            <form className="flex flex-col bg-white shadow-xl/30 p-6 gap-4 w-70">
+            <form  onSubmit={validate} noValidate
+            
+            className="flex flex-col bg-white shadow-xl/30 p-6 gap-4 w-70">
                 <h1 className="text-center text-sky-600 text-xl font-bold">Login</h1>
                 <label>Username</label>
                 <input className="border p-1 rounded "
@@ -14,7 +50,7 @@ const [password, setPass] = useState("");
                 value={username}
                 onChange={(e)=>setUsername(e.target.value)}
                 />
-
+            <p>{error}</p>
                 <label>Password</label>
                 <input  className="border p-1 rounded "
                 type="password"
@@ -22,7 +58,7 @@ const [password, setPass] = useState("");
                 value={password}
                 onChange={(e)=>setPass(e.target.value)}
                 />
-
+            <p>{error}</p>
                 <button className="bg-sky-600 p-1 rounded-xl">Login</button>
             </form>
         </div>

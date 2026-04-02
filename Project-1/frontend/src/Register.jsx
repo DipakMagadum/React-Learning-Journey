ea9f76874239131e9ec7581fc4a57da23f2aada0
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-02 22:56:32 +0530

    add input component in Register

--- Project-1/frontend/src/Register.jsx
+++ Project-1/frontend/src/Register.jsx
@@ -1,3 +1,5 @@
+import { Input } from "./Components/Input";
+
 export function Register(){
     return(
         <>
@@ -6,6 +8,13 @@ export function Register(){
             <form className="flex flex-col bg-white shadow-xl/50 w-90 rounded-xl p-6">
 
             <h1 className="text-center text-sky-600 font-bold text-xl ">Register</h1>
+
+            <Input
+            label="Username"
+            type=""
+            value=""
+            onChange=""
+            />
 
             </form>
         </div>

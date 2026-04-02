64a184cf948f56998f0296786bc576810f7629ec
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-02 23:22:24 +0530

    created reusable Input component with validation

--- Project-1/frontend/src/Components/Input.jsx
+++ Project-1/frontend/src/Components/Input.jsx
@@ -1,15 +1,26 @@
-import {useState} from "react"
-export function Input({label}){
+import {useState,useEffect} from "react"
+export function Input({label,placeholder}){
 
     const [input,setInput] = useState("")
+    const [error,setError] = useState("")
+
+    useEffect(()=>{
+        
+        if(input === ""){
+            setError(" is Required")
+        }
+        
+    },[input])
     return(
         <>
         <label>{label}</label>
         <input className="border rounded p-1"
         type="text"
+        placeholder={placeholder}
         value={input}
         onChange={(e)=>setInput(e.target.value)}
         />
+        {error && <p className="text-red-600">{label}{error}</p>}
         </>
     )
 }

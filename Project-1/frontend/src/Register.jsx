346ae55a1b652e242ad6eb618fb36116fc24b784
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-02 22:43:35 +0530

    added style in Register.jsx

--- Project-1/frontend/src/Register.jsx
+++ Project-1/frontend/src/Register.jsx
@@ -1,8 +1,13 @@
 export function Register(){
     return(
         <>
-        <div>
-            <form></form>
+        <div className="flex justify-center items-center h-screen">
+
+            <form className="flex flex-col bg-white shadow-xl/50 w-90 rounded-xl p-6">
+
+            <h1 className="text-center text-sky-600 font-bold text-xl ">Register</h1>
+
+            </form>
         </div>
         </>
     )

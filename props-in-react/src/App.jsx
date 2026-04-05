42f1d325c20375ebbedfa0df3a768d9dfa9cfd48
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-05 16:31:16 +0530

    render Navbar component

--- props-in-react/src/App.jsx
+++ props-in-react/src/App.jsx
@@ -1,8 +1,12 @@
 
 import Card from "./Components/Card"
+import Navbar from "./Components/Navbar"
 function App() {
   return (
    <>
+
+   <Navbar items={["Home","About us","Login"]}
+   />
    <div className="flex ml-96 mt-52">
   <h1 className='bg-green-400 text-white text-center text-xl p-4 rounded-xl w-80 '>Props In React</h1>
     </div>
@@ -11,7 +15,9 @@ function App() {
     name="Naruto"
     btnText="Click"
     />
-
+    <br/>
+<Navbar items={["Home","Register","Login"]}
+   />
     <Card
     name="Sasuke"
     btnText="click Me"

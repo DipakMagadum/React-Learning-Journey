5cfdb666d037d3499466b39f126e2f715ff10a45
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-07 18:49:06 +0530

    completed the project

--- Project-2/bgChanger/src/App.jsx
+++ Project-2/bgChanger/src/App.jsx
@@ -1,7 +1,53 @@
+import {useState} from "react"
 function App() {
+  const [color , setColor] = useState("black")
+
   return (
     <>
-    <h1 className="text-xl text-center mt-10">Background Changer</h1>
+    <div className="w-full h-screen duration-0" 
+      style={{backgroundColor:color}}
+    >
+    
+    <div
+     className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 " >
+      
+      <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl">
+
+
+        <button onClick={()=>setColor("red")}
+        
+        className="outline-none px-4 py-1 rounded-full  shadow-xl"
+        style={{backgroundColor:"red"}}
+        >Red</button>
+
+         <button  onClick={()=>setColor("Orange")} 
+         
+         className="outline-none px-4 py-1 rounded-full  shadow-xl"
+        style={{backgroundColor:"Orange"}}
+        >Orange</button>
+
+         <button onClick={()=>setColor("green")}
+          
+         className="outline-none px-4 py-1 rounded-full  shadow-xl"
+        style={{backgroundColor:"Green"}}
+        >Green</button>
+
+         <button onClick={()=>setColor("yellow")}
+         
+         className="outline-none px-4 py-1 rounded-full  shadow-xl"
+        style={{backgroundColor:"yellow"}}
+        >Yellow</button>
+
+         <button onClick={()=>setColor("aqua")}
+         
+         className="outline-none px-4 py-1 rounded-full  shadow-xl"
+        style={{backgroundColor:"aqua"}}
+        >Aqua</button>
+      </div>
+      </div>
+
+    </div>
+
     </>
   )
 }

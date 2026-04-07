312f56868ce4f0ee7419e3a1ad4724a13744f476
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-07 09:09:13 +0530

    create Button component

---
+++ props-in-react/src/Components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button({btnText}) {
+  return (
+    <>
+    <button className="bg-sky-500 p-2 text-center text-white">{btnText}</button>
+    </>
+  )
+}
+
+export default Button

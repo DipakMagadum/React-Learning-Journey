576b2b4b4362df5a9237ea86a6a7bbf698a025e3
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-09 19:04:48 +0530

    import useCallback hook

--- PasswordGenrator/src/App.jsx
+++ PasswordGenrator/src/App.jsx
@@ -1,10 +1,11 @@
-import {useState} from "react"
+import {useState,useCallback} from "react"
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
 
+  const passwordGenrator =useCallback([length,numberAllowed,charAllowed,setPassword])
   return (
    <>
    <h1 className="text-4xl text-center">PasswordGenrator</h1>

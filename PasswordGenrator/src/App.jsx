029d49e10fd13c85083b3ba6c94d26edef641e15
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-09 19:43:32 +0530

    create a input field

--- PasswordGenrator/src/App.jsx
+++ PasswordGenrator/src/App.jsx
@@ -5,11 +5,39 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
 
-  const passwordGenrator =useCallback([length,numberAllowed,charAllowed,setPassword])
-  return (
-   <>
-   <h1 className="text-4xl text-center">PasswordGenrator</h1>
+  const passwordGenrator =useCallback(()=>{
 
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz"
+
+    if(numberAllowed) str+="0123456789"
+    if(charAllowed) str+="!@#$%^&*"
+
+    for(let i=1; i<= array.length; i++){
+
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+
+    }
+    setPassword(pass)
+
+  },[length,numberAllowed,charAllowed,setPassword])
+
+  return (
+   <> 
+  <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-red-600 bg-gray-700">
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+
+      <input 
+      type="text"
+      value={password}
+      className="outline-none w-full py-1 px-3"
+      placeholder="password"
+      readOnly
+
+      />
+    </div>
+  </div>
    </>
   )
 }

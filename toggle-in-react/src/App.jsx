47b49782809cfb660a3b6871f59a1f6e018e9064
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-10 19:05:23 +0530

    write the logic of toggle button

--- toggle-in-react/src/App.jsx
+++ toggle-in-react/src/App.jsx
@@ -1,7 +1,13 @@
+import {useState} from "react"
 function App() {
+  const [show, setShow] = useState(false)
   return (
   <>
-  
+    <h1>Toggle button in React using useState</h1>
+
+    <button onClick={()=>setShow(!show)}>Show/hide</button>
+
+    {show ? <h3>hello</h3>:null}
   </>
   )
 }

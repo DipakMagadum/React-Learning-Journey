4e156ed89bef5db82598fa04a3541f277ce2505c
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-10 19:12:11 +0530

    create home.jsx

---
+++ toggle-in-react/src/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+function Home() {
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas aliquid aspernatur vel esse odit, optio nesciunt pariatur amet unde eius. Aliquam omnis reprehenderit modi ducimus ratione fuga in et animi!
+    Et reiciendis esse accusantium eligendi ratione odit, nostrum quibusdam dolores fuga repellendus architecto dolorem id fugiat sapiente eveniet ut maiores laborum! Sint enim sed officia cupiditate, tempora cumque ipsum omnis?
+    Commodi similique tempore, qui earum, laudantium, dolores necessitatibus ipsa enim placeat debitis esse hic. Et aliquid voluptates cum libero natus suscipit! Rem iusto ipsum ad, natus repudiandae iste quae provident.
+    Nostrum et laboriosam aperiam adipisci! Unde quis culpa minima quidem voluptates exercitationem odit facere similique. Praesentium, aperiam debitis. Eius voluptates nobis tenetur neque! Necessitatibus molestias, iusto in voluptates perferendis excepturi?
+    Asperiores necessitatibus adipisci distinctio qui atque pariatur sint unde eos magnam alias! Consequuntur dolores odio aperiam dicta, nesciunt vero non minus eveniet pariatur ipsa tempora maiores quae quisquam eius explicabo.
+    Autem voluptas eveniet, debitis expedita non aliquam sit possimus dolores adipisci optio itaque. Reiciendis ducimus dolore deserunt accusantium, quae, rerum itaque accusamus veniam harum amet aspernatur velit laboriosam neque sed?
+    Molestiae suscipit et maxime eligendi quis enim exercitationem, aut laboriosam a eaque dolores! Dolorum, vitae fuga doloremque placeat quos voluptas autem accusamus modi repudiandae distinctio ullam, voluptates eius reiciendis totam.
+    Laboriosam fuga pariatur autem, sunt numquam ullam. Corporis, totam recusandae aliquid et fugit beatae minima in enim reprehenderit eaque nobis nesciunt nulla dolor explicabo, eius placeat harum maxime quasi ipsam.
+    Aut quaerat maiores dolorem, eligendi nesciunt libero minus laudantium sed veniam reiciendis fugiat commodi quasi quis fugit, tenetur labore debitis. Nulla est dicta adipisci nam, nemo dolor cum reprehenderit dolore?
+    Provident expedita cumque officiis dicta, quasi natus est numquam suscipit vitae, excepturi facere dolorem nemo velit quam harum aspernatur enim fugit! Deserunt alias saepe, maxime vitae et sed ex mollitia!</p>
+    </>
+  )
+}
+
+export default Home

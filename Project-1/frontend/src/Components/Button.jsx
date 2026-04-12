0a19eb2481582d44cfdfbc02b4e86b0fc258c7f9
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-12 19:22:38 +0530

    Create Button component

---
+++ Project-1/frontend/src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Button({name}) {
+  return (
+    
+        <button className="bg-sky-500">
+            {name}
+            </button>
+  )
+}
+
+export default Button

efec351398fc68ce42b07bcc2a20ff4207c643a0
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-13 10:48:33 +0530

    fix: position of button ui

--- Project-1/frontend/src/Components/Button.jsx
+++ Project-1/frontend/src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button({name}) {
   return (
     
-        <button className="bg-sky-500">
+        <button className="bg-sky-500 mt-5 w-full p-2 rounded-xl text-white text-xl">
             {name}
             </button>
   )

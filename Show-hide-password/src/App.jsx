496346b6541ddbb94eb31f77a47fa2133f6cd800
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-14 12:02:06 +0530

    add password component

--- Show-hide-password/src/App.jsx
+++ Show-hide-password/src/App.jsx
@@ -1,9 +1,21 @@
-import {useState} from "react"
+import Password from "./Password"
+
 function App() {
   return (
    <>
    <div className=" flex bg-sky-400 mt-5 justify-center">
-    <h2 className="p-4 text-xl text-center">Show Hide Password Icon </h2></div>
+    <h2 className="p-4 text-xl text-center">Show Hide Password Icon </h2>
+      </div>
+
+       <div className="flex  justify-center mt-5  ">  
+
+        <form className="   flex flex-col w-72 bg-white  border border-black  rounded-xl p-6 gap-2 ">
+           <Password
+         label="Enter password"
+         />
+        </form>
+    </div>
+
    
    </>
   )

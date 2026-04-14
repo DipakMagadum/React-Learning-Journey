c236b1e218cb3c77bfe39e56885acf5be52d823e
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-14 13:29:34 +0530

    add  import lucide-react and feature Eye and EyeOff

--- Show-hide-password/src/Password.jsx
+++ Show-hide-password/src/Password.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import {useState} from "react"
 
+import {useState} from "react"
+import {Eye, EyeOff} from "lucide-react"
 function Password({label}) {
 
     const [showPassword, setShowPassword] = useState(false);
-    
+
   return (
     
     <>
     <label>{label}</label>
-    <input className="border border-black rounded p-1  "
-    type="password"
+    <input className="  border border-black rounded p-1  "
+    type={showPassword?"text":"password"} //this is a ternary operator Syntax: condition ? value_if_true : value_if_false.
     placeholder="password"
 
     />
+    <div>
+         <button 
+         type="button"
+         onClick={()=>setShowPassword(!showPassword)}
+         >
+           {showPassword?<Eye size={20}/>:<EyeOff size={20}/>}
+         </button>
+    </div>
+   
     </>
   )
 }

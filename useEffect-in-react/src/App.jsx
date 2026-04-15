8811bc4c907203f54b85cea764df19ff05acec59
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-15 17:17:38 +0530

    feat: add two buttons

--- useEffect-in-react/src/App.jsx
+++ useEffect-in-react/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { useState,useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [Countreverse,setCountReverse] =useState(0);
 
+  useEffect(()=>{
+     hello();
+  },[])
+
+  function hello() {
+    console.log("useEffect in react")
+  }
+ 
   return (
     <>
-    
+    <h1>useEffect In react</h1>
+
+    <button onClick={()=>setCount(count+1)}>Count{count}</button>
+    <button onClick={()=>setCountReverse(Countreverse-1)}>ReverseCount{Countreverse}</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

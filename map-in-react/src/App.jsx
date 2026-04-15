e6194a95aaa17af0f8424b14e589b06b5320c3e1
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-15 13:16:04 +0530

    refactor: optimize student component

--- map-in-react/src/App.jsx
+++ map-in-react/src/App.jsx
@@ -1,15 +1,10 @@
 import StudentDetail from "./StudentDetail"
-
 function App() {
-
-
   return (
     <>
-
     <h1 className="text-center mt-5 mb-5">Student detail using Map </h1>
     <StudentDetail/>
     </>
   )
 }
-
 export default App

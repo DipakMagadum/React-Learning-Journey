762ce225d3ade9942381b02558eaa0d372f6cfd7
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-15 20:53:49 +0530

    style: improve button layout and alignment

--- useEffect-in-react/src/App.jsx
+++ useEffect-in-react/src/App.jsx
@@ -6,18 +6,26 @@ function App() {
 
   useEffect(()=>{
      hello();
-  },[])
+  },[count])
 
   function hello() {
     console.log("useEffect in react")
+    
   }
  
   return (
     <>
-    <h1>useEffect In react</h1>
+    <h1 className=" text-2xl text-center mt-5 font-semibold text-fuchsia-500">useEffect In react</h1>
 
-    <button onClick={()=>setCount(count+1)}>Count{count}</button>
-    <button onClick={()=>setCountReverse(Countreverse-1)}>ReverseCount{Countreverse}</button>
+   <div className="flex justify-center items-center   p-3">
+    <div className="flex flex-col w-52 gap-2 border p-4">
+       <button className="bg-sky-500 p-1 rounded-lg text-center text-white"
+    onClick={()=>setCount(count+1)}>Count {count}</button>
+
+    <button  className="bg-sky-500 p-1 rounded-lg text-center text-white"
+    onClick={()=>setCountReverse(Countreverse-1)}>ReverseCount {Countreverse}</button>
+    </div>
+   </div>
     </>
   );
 }

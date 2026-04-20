fe592e6797acc5d956ef19c0c2b15b260b828906
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-20 23:29:35 +0530

    feat: add Nav Link in navbar

--- Router-in-react/src/Home.jsx
+++ Router-in-react/src/Home.jsx
@@ -1,12 +1,13 @@
+import {Link} from "react-router-dom"
 export function Home(){
     return(
         <>
         <div className="flex bg-sky-600 h-15 items-center justify-end p-4 ">
         <nav>
             <ul className="flex gap-7 text-white text-lg">
-                <li>Login</li>
-                <li>Register</li>
-                <li>About</li>
+              <Link to="/login"> <li>Login</li></Link> 
+               <Link to="/register"> <li>Register</li> </Link>
+                <Link to="/about"><li>About</li></Link>
             </ul>
         </nav>
         </div>

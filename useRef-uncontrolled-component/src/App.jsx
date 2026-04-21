27d5345a69bd95f0f9d9d70b06027dd464fe801c
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-21 20:13:16 +0530

    feat: handle radio button selection using useRef in uncontrolled form

--- useRef-uncontrolled-component/src/App.jsx
+++ useRef-uncontrolled-component/src/App.jsx
@@ -1,42 +1,84 @@
-import {useRef} from "react"
+import { useRef } from "react";
 function App() {
-
   const nameRef = useRef(null);
-   const passwordRef = useRef(null);
+  const passwordRef = useRef(null);
+  const maleRef = useRef(null);
+  const FemaleRef = useRef(null);
 
-   const FormHandler = (e) =>{
+  const FormHandler = (e) => {
     e.preventDefault();
-    nameRef.current.style.color="red";
-    passwordRef.current.style.color="red";
+    nameRef.current.style.color = "red";
+    passwordRef.current.style.color = "red";
 
     const userName = nameRef.current.value;
     const password = passwordRef.current.value;
 
-    alert(`username is ${userName} and password is ${password}`)
-   }
+    let genderSelector = "";
+
+    if (maleRef.current.checked) {
+      genderSelector = maleRef.current.value;
+    } else if (FemaleRef.current.checked) {
+      genderSelector = FemaleRef.current.value;
+    }
+    alert(
+      `username is ${userName} and password is ${password} and gender is ${genderSelector}`,
+    );
+  };
   return (
     <>
-    <div className="flex justify-center items-center h-screen">
-      <form action="" method="Post"
-        onSubmit={FormHandler}
-        className="flex flex-col bg-white shadow-2xl w-90 px-5 py-3  gap-2 rounded-lg border border-gray-300"
-      >
-        <h1>Getting Value From TextBox</h1>
-        <label >Name</label>
-        <input
-          className="border border-black rounded p-1"
-         type="text"   ref={nameRef} placeholder="name"/>
-          <label >Password</label>
-         <input 
-          className="border border-black rounded p-1"
-           type="password" placeholder="Password"
+      <div className="flex justify-center items-center h-screen">
+        <form
+          action=""
+          method="Post"
+          onSubmit={FormHandler}
+          className="flex flex-col bg-white shadow-2xl w-90 px-5 py-3  gap-2 rounded-lg border border-gray-300"
+        >
+          <h1 className="bg-purple-400  p-1">Getting Value From TextBox</h1>
+          <label>Name</label>
+          <input
+            className="border border-black rounded p-1"
+            type="text"
+            ref={nameRef}
+            placeholder="name"
+          />
+
+          <label>Password</label>
+          <input
+            className="border border-black rounded p-1"
+            type="password"
+            placeholder="Password"
             ref={passwordRef}
-         />
-         <button className="bg-sky-500 rounded-lg p-1 text-white">Submit</button>
-      </form>
-    </div>
+          />
+
+          {/* radioButton */}
+          <h1 className="bg-purple-400  p-1 ">Getting Value From RadioButtons</h1>
+          <label>Gender</label>
+          <div className="flex  gap-2">
+            <input
+              className="cursor-pointer"
+              type="radio"
+              value="Male"
+              name="gender"
+              ref={maleRef}
+            />
+            Male
+            <input
+              className="cursor-pointer"
+              type="radio"
+              value="Female"
+              name="gender"
+              ref={FemaleRef}
+            />
+            Female
+          </div>
+
+          <button className="bg-sky-500 rounded-lg p-1 text-white">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;

55e1182f5d3d23adec01c2fe5ef2f603007e65d2
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-22 22:03:54 +0530

    feat: create initial layout for controlled component form

--- useState-controlled-component/src/App.jsx
+++ useState-controlled-component/src/App.jsx
@@ -2,6 +2,13 @@ function App() {
 
   return (
     <>
+    <div className="flex justify-center items-center h-screen">
+      <form action=""
+        className="flex flex-col bg-white  shadow-2xl border border-gray-400 rounded-lg p-4 gap-3 w-[300px]"
+      >
+        <h1 className="text-center bg-lime-300 rounded p-1 font-semibold">Controlled Component</h1>
+      </form>
+    </div>
     </>
   )
 }

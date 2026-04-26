514f23a483bd94abd1d0f9e0200eeacee1c3ec3d
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-26 19:59:18 +0530

    feat: implement controlled radio button handling using useState

--- useState-controlled-component/src/Component/RadioButton.jsx
+++ useState-controlled-component/src/Component/RadioButton.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-
+import React from 'react';
+import {useState} from "react";
 function RadioButton() {
+
+    const [gender, setGender] = useState("");
+
+    const handleGender = (e)=>{
+        setGender(e.target.value);
+    }
   return (
    <>
    <label htmlFor="">Gender</label> 
    <div className="flex gap-5">
+
+    {/* radiobutton for male */}
     <label htmlFor="male">Male
-        <input type="radio" value={"Male"} name="Gen" id="male"/>
+        <input type="radio"
+         value={"Male"}
+          name="Gen"
+           id="male"
+           onChange={handleGender}/>
     </label>
 
+    {/* radiobutton for female */}
     <label htmlFor="female">Female
-        <input type="radio" value={"Female"} name="Gen" id="female"/>
+        <input type="radio"
+         value={"Female"} 
+         name="Gen"
+          id="female"
+           onChange={handleGender}/>
     </label>
-
    </div>
+   <h4>You are:{gender}</h4>
    </>
   )
 }

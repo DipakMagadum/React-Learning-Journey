3746408d83ca3ee3e02276f49a0e2d47f265510e
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-27 20:44:47 +0530

    feat: add dropdown state handling and display selected city

--- useState-controlled-component/src/Component/DropDown.jsx
+++ useState-controlled-component/src/Component/DropDown.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-
+import {useState} from "react"
 function DropDown() {
+
+    const [city, setCity] = useState("");
+
+    const handleCity = (e)=>{
+        setCity(e.target.value);
+    }
   return (
    <>
    <label >Select City</label>
-   <select name="" id="">
+   <select onChange={handleCity}>
     <option value="pune">Pune</option>
     <option value="mumbai">Mumbai</option>
     <option value="delhi">Delhi</option>
     <option value="kolhapur">Kolhapur</option>
    </select>
+   <h4> selected city is : {city}</h4>
    </>
   )
 }

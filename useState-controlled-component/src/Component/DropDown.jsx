cf59eb4217d2d56f7b9fa5626483907a383c086c
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-27 20:39:31 +0530

    feat: add dropdown component for city selection

---
+++ useState-controlled-component/src/Component/DropDown.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function DropDown() {
+  return (
+   <>
+   <label >Select City</label>
+   <select name="" id="">
+    <option value="pune">Pune</option>
+    <option value="mumbai">Mumbai</option>
+    <option value="delhi">Delhi</option>
+    <option value="kolhapur">Kolhapur</option>
+   </select>
+   </>
+  )
+}
+
+export default DropDown

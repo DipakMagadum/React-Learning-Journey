0cf4cd377f680ef475866c08782dc7a324860076
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-27 21:03:43 +0530

    chore: initialize react api integration project with vite

---
+++ react-api-integration/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+ 
+  return (
+    <>
+    <h1 className="text-center bg-sky-500 mt-5 p-3 text-white font-semibold text-2xl ">Fetch and Display From API Using Get Method</h1>
+    </>
+  )
+}
+
+export default App

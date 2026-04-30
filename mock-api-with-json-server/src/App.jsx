01cf1cf62e3ad06804f8c8daf39e89918cd9e032
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-30 20:44:06 +0530

    feat: integrate json-server API and log users data

--- mock-api-with-json-server/src/App.jsx
+++ mock-api-with-json-server/src/App.jsx
@@ -1,6 +1,20 @@
-
+import {useEffect} from "react";
 function App() {
 
+   useEffect (()=>{
+      usersData()
+    },[])
+
+  async function usersData (){
+
+   
+    const url = "http://localhost:3000/users"
+
+    let response = await fetch(url)
+    response = await response.json()
+    console.log(response)
+
+  }
   return (
    <>
    </>

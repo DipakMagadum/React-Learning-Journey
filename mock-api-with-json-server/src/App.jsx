cc9d590e149c5e6111132dee43a9e59b74d743c7
DipakMagadum <deepakmagdum769@gmail.com>
2026-04-30 22:31:13 +0530

    feat: render users data in UI with Tailwind styling

--- mock-api-with-json-server/src/App.jsx
+++ mock-api-with-json-server/src/App.jsx
@@ -1,6 +1,10 @@
 import {useEffect} from "react";
+import {useState} from "react";
 function App() {
 
+    const [userData, setUserData] = useState([]);
+
+  
    useEffect (()=>{
       usersData()
     },[])
@@ -13,10 +17,27 @@ function App() {
     let response = await fetch(url)
     response = await response.json()
     console.log(response)
+    setUserData(response)
+    
 
   }
   return (
    <>
+   {
+     userData.map((users)=>(
+
+      <div 
+      key={userData.id}
+      className="flex justify-center items-center mt-10 ">
+         <ul className="bg-white shadow-xl p-3 rounded border border-gray-600">
+        <li>Id : {users.id}</li>
+        <li>Name : {users.name}</li>
+        <li>Email : {users.email}</li>
+        <li>City : {users.city}</li>
+      </ul>
+   </div>
+    ))
+  }
    </>
   )
 }

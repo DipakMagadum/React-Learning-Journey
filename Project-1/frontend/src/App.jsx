c66e82d9b235b0ada9e7109aa060cf6e726bc65e
DipakMagadum <deepakmagdum769@gmail.com>
2026-05-17 17:11:28 +0530

    feat: configure login page routing

--- Project-1/frontend/src/App.jsx
+++ Project-1/frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import {Routes,Route} from "react-router-dom"
 import { Register } from "./Register"
+import {Login} from "./Login"
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<Register/>} />
+      <Route path="/login" element={<Login/>}/>
     </Routes>
     </>
   )

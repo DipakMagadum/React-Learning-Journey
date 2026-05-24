7fed9a79271f22604d588a5995ab7537051b0520
DipakMagadum <deepakmagdum769@gmail.com>
2026-05-24 19:34:13 +0530

    feat: implement user registration page with Tailwind CSS

---
+++ Router-in-react/src/Register.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <>
+    <div className='flex justify-center items-center h-screen'>
+        <div className= 'flex flex-col bg-white shadow-2xl w-80  gap-3 p-7 '>
+            <h1 className='text-center text-sky-600 font-semibold font-serif'>Register</h1>
+            <label >First Name</label>
+            <input
+            className='border border-gray-400 rounded-sm p-1'
+            type='text'
+            placeholder='First Name'
+            />
+
+            <label >Last Name</label>
+            <input
+            className='border border-gray-400 rounded-sm p-1'
+            type='text'
+            placeholder='Last Name'
+            />
+
+             <label >Email</label>
+            <input
+            className='border border-gray-400 rounded-sm p-1'
+            type='email'
+            placeholder='Email'
+            />
+
+             <label >Password</label>
+            <input
+            className='border border-gray-400 rounded-sm p-1'
+            type='password'
+            placeholder='Password'
+            />
+
+             <label >Confirm Password</label>
+            <input
+            className='border border-gray-400 rounded-sm p-1'
+            type='password'
+            placeholder='Confirm Password'
+            />
+            <button className='bg-sky-600 py-1 px-3 rounded-2xl text-white font-serif'>Register</button>
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Register
